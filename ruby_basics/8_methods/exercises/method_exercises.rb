e25ae13a1218b67e41d10acabbf4957d82c4b73a
Justin Townley <justin.d.townley@gmail.com>
2022-07-24 23:41:22 -0700

    Run though some exercises in 06 Methods

--- ruby_basics/8_methods/exercises/method_exercises.rb
+++ ruby_basics/8_methods/exercises/method_exercises.rb
@@ -6,23 +6,35 @@
 # parameter: number (an integer)
 # return value: the number's ASCII character (https://www.ascii-code.com/)
 # hint: use Integer#chr
+def ascii_translator(number)
+    number.chr
+end
 
 
 # method name: #common_sports
 # parameters: current_sports and favorite_sports (both arrays)
 # return value: an array containing items in both arrays
 # hint: use Array#intersection
+def common_sports(current_sports, favorite_sports)
+    current_sports.intersection(favorite_sports)
+end
 
 
 # method name: #alphabetical_list
 # parameter: games (an array)
 # return value: games, alphabetically sorted and duplicates removed
 # hint: chain Array#sort and Array#uniq together
+def alphabetical_list(games)
+    games.sort.uniq
+end
 
 
 # method name: #lucky_number
 # parameter: number (an integer) with default value of 7
 # return value: a string "Today's lucky number is <number>"
+def lucky_number(number = 7)
+    "Today's lucky number is #{number}"
+end
 
 
 # method name: #ascii_code
@@ -30,6 +42,14 @@
 # return value: the character's ordinal number
 # explicit return value: 'Input Error' if character's length does not equal 1
 # hint: use String#ord
+def ascii_code(string)
+    ordinal = string.ord
+    if (ordinal != 1)
+        return 'Input Error'
+    else 
+        return ordinal
+    end
+end
 
 
 # method name: #pet_pun
